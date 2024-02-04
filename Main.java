2c2baddba9a458e2536f81375003b10e87c5d59a
rodionotelli <148154207+rodionotelli@users.noreply.github.com>
2024-02-04 22:51:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
+        int c = calc.devide.apply(a, b); // исправлена ошибка из-за возможного деления на ноль
 
         calc.println.accept(c);
     }
